426799bbe533905278f2a8b902145505cb70e199
Klein <klein.gachuhi@student.moringaschool.com>
2025-03-15 03:05:26 +0300

    Changes

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+// const destructivelyAppendCat = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat(){
+    return cats.push('Ralph')
+};
+
+function destructivelyPrependCat(){
+    return cats.unshift('Bob')
+};
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+};
+
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+};
+
+function removeFirstCat() {
+    return cats.slice(1, 3)
+};
+console.log(cats);
+
+function removeLastCat() {
+    return cats.slice(0, 2)
+};
+
+function appendCat() { 
+ const newCatsName =[...cats, 'Broom'];
+ return newCatsName
+};
+appendCat()
+
+function prependCat() {
+    const newKittyName =['Arnold', ...cats]
+    return newKittyName;
+};
+console.log(prependCat())
